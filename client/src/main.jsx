a12607a3b339a636c73038f2f70dbe74752205b3
Sahil Raj Verma <sahilrv196@gmail.com>
2026-08-21 15:38:56 +0530

    feat: mount interactive 3D hero on public site

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,14 @@ import AdminApp from './admin/AdminApp.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 import ScrollChoreography from './components/ScrollChoreography.jsx'
 import PageTransition from './components/PageTransition.jsx'
+import Hero3D from './components/Hero3D.jsx'
 import './styles/index.css'
 import './admin/admin.css'
 
 const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/')
 
 function PublicApp() {
-  return <BrowserRouter><PageTransition/><Routes><Route path="/" element={<App />} /><Route path="/work/:slug" element={<ProjectPage />} /></Routes></BrowserRouter>
+  return <BrowserRouter><PageTransition/><Routes><Route path="/" element={<App />} /><Route path="/work/:slug" element={<ProjectPage />} /></Routes><Hero3D /></BrowserRouter>
 }
 
 function Root() {
