5642e1a1d6a72bca1d66a03009867da611e0c043
Sahil Raj Verma <sahilrv196@gmail.com>
2026-08-21 18:35:38 +0530

    fix: load dedicated contact form styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,6 +13,7 @@ import HeaderEnhancer from './components/HeaderEnhancer.jsx'
 import './styles/index.css'
 import './styles/accessibility.css'
 import './styles/responsive.css'
+import './styles/contact.css'
 import './admin/admin.css'
 
 const isAdminRoute =
