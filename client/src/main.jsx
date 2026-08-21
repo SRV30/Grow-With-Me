98774158e7a23e76df028f2b14068e9ed8733ac3
Sahil Raj Verma <sahilrv196@gmail.com>
2026-08-21 15:13:02 +0530

    feat: mount protected admin application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminApp from './admin/AdminApp.jsx'
 import './styles/index.css'
+import './admin/admin.css'
+
+const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAdminRoute ? <AdminApp /> : <App />}
   </React.StrictMode>,
 )
