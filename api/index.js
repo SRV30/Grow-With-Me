ea6c7586afff8ce35c6095b973451172aa658b9a
Sahil Raj Verma <sahilrv196@gmail.com>
2026-08-21 22:28:09 +0530

    feat: connect MongoDB for Vercel serverless API

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
 import app from '../server/src/app.js'
+import { connectDatabase } from '../server/src/config/db.js'
 
-export default app
+let databasePromise
+
+const ensureDatabase = async () => {
+  if (!databasePromise) {
+    databasePromise = connectDatabase().catch((error) => {
+      databasePromise = undefined
+      throw error
+    })
+  }
+
+  return databasePromise
+}
+
+export default async function handler(req, res) {
+  await ensureDatabase()
+  return app(req, res)
+}
