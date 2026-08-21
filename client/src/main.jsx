ce7b4af3ea9f7b88d9c99529c61ab16432c8a4b5
Sahil Raj Verma <sahilrv196@gmail.com>
2026-08-21 15:45:16 +0530

    feat: wire premium work index route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import WorkPage from './pages/WorkPage.jsx'
 import ProjectPage from './pages/ProjectPage.jsx'
 import AdminApp from './admin/AdminApp.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
@@ -14,13 +15,11 @@ import './admin/admin.css'
 const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/')
 
 function PublicApp() {
-  return <BrowserRouter><PageTransition/><Routes><Route path="/" element={<App />} /><Route path="/work/:slug" element={<ProjectPage />} /></Routes><Hero3D /></BrowserRouter>
+  return <BrowserRouter><PageTransition/><Routes><Route path="/" element={<App />} /><Route path="/work" element={<WorkPage />} /><Route path="/work/:slug" element={<ProjectPage />} /></Routes><Hero3D /></BrowserRouter>
 }
 
 function Root() {
   return <ErrorBoundary>{isAdminRoute ? <AdminApp /> : <><PublicApp /><ScrollChoreography /></>}</ErrorBoundary>
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><Root /></React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><Root /></React.StrictMode>)
