143ddcf330c3f6003b70c4efce663cfc3734f6d6
Sahil Raj Verma <sahilrv196@gmail.com>
2026-09-04 10:52:53 +0530

    Add admin user management routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { login, logout, me } from '../controllers/auth.controller.js'
-import { changeAdminPassword, createAdminUser } from '../controllers/adminUser.controller.js'
+import { changeAdminPassword, createAdminUser, deleteAdminUser, listAdminUsers } from '../controllers/adminUser.controller.js'
 import { requireAuth } from '../middleware/auth.js'
 import { loginRateLimit } from '../middleware/rateLimits.js'
 
@@ -9,7 +9,9 @@ const router = Router()
 router.post('/login', loginRateLimit, login)
 router.post('/logout', logout)
 router.get('/me', requireAuth, me)
+router.get('/users', requireAuth, listAdminUsers)
 router.post('/users', requireAuth, createAdminUser)
+router.delete('/users/:userId', requireAuth, deleteAdminUser)
 router.patch('/password', requireAuth, changeAdminPassword)
 
 export default router
