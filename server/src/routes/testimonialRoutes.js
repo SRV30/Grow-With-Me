284585443976e966398f225e2c2f3feae1281bc1
Sahil Raj Verma <sahilrv196@gmail.com>
2026-09-06 16:09:24 +0530

    Add testimonial routes

---
+++ server/src/routes/testimonialRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { listTestimonials, getTestimonial, createTestimonial, updateTestimonial, deleteTestimonial } from '../controllers/testimonialController.js'
+import { protect } from '../middleware/auth.js'
+
+const router = express.Router()
+
+router.get('/', listTestimonials)
+router.get('/:id', protect, getTestimonial)
+router.post('/', protect, createTestimonial)
+router.put('/:id', protect, updateTestimonial)
+router.delete('/:id', protect, deleteTestimonial)
+
+export default router
